9ed1dd660eefacbb2579332772109e7e1b0efdec
thgpecs <thgpecs@gmail.com>
2018-03-24 22:17:56 +0530

    categories are added iamges can be chosen even from the gallery

--- app/src/main/java/ssn/codebreakers/pecsinstructor/ImagePickerExampleActivity.java
+++ app/src/main/java/ssn/codebreakers/pecsinstructor/ImagePickerExampleActivity.java
@@ -1,6 +1,17 @@
 package ssn.codebreakers.pecsinstructor;
+        import android.content.DialogInterface;
+        import android.support.v7.app.AlertDialog;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.EditText;
+        import android.widget.FrameLayout;
+        import android.widget.GridView;
+        import java.util.ArrayList;
+        import java.util.List;
 
-import android.content.Intent;
+        import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -8,59 +19,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+        import android.widget.LinearLayout;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import ssn.codebreakers.pecsinstructor.helpers.Callback;
-import ssn.codebreakers.pecsinstructor.helpers.ImagePicker;
+        import ssn.codebreakers.pecsinstructor.db.helpers.CategoryHelper;
+        import ssn.codebreakers.pecsinstructor.db.models.Category;
+        import ssn.codebreakers.pecsinstructor.helpers.Callback;
+        import ssn.codebreakers.pecsinstructor.helpers.CommonUtils;
+        import ssn.codebreakers.pecsinstructor.helpers.ImagePicker;
 
 /*
     Example Activity to show the use of ImagePicker API
  */
 public class ImagePickerExampleActivity extends AppCompatActivity {
 
-    ImagePicker imagePicker;
-
-    Button imagePickerButton;
-    ImageView selectedImageView;
+    GridView gridView ;
+    ArrayAdapter<String> adapter;
+    static final String[ ] GRID_DATA = new String[] {
+           "fruits" ,
+            "veges",
+            "animals" ,
+            "daily routines",
+            "food" ,
+            "aches & pains",
+            "relatives",
+            "things in pairs",
+            "smell" ,
+            "simple words"
+    };
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_picker_example);
+        setContentView( R.layout.gridviewfile );
 
-        imagePickerButton = findViewById(R.id.selectImageButton);
-        selectedImageView = findViewById(R.id.selectedImage);
-
-        imagePicker = new ImagePicker(this);
-
-        imagePickerButton.setOnClickListener(new View.OnClickListener() {
+        final Button addcategory = findViewById(R.id.addc);
+        addcategory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imagePicker.chooseImage(new Callback() {
-                    @Override
-                    public void onSuccess(Object result) {//returns the file path of the selected image
-                        String filePath = (String)result;
-                        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
-                        selectedImageView.setImageBitmap(bitmap);
-                    }
+                startActivity(new Intent(getApplicationContext(), category_pic.class));
+                }
 
-                    @Override
-                    public void onError(Object object) {
-                    }
-                });
+
+        }
+        );
+
+        // Get gridview object from xml file
+
+        gridView = (GridView) findViewById(R.id.gridView1);
+
+
+        // Set custom adapter (GridAdapter) to gridview
+
+        gridView.setAdapter(  new CustomGridAdapter( this, GRID_DATA ) );
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        ((TextView) v.findViewById( R.id.textView ))
+                                .getText(), Toast.LENGTH_SHORT).show();
+
             }
         });
+
     }
 
-    //must be added in the activity in which imagepicker is used
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-        imagePicker.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-    }
 
-    //must be added in the activity in which imagepicker is used
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        imagePicker.onRequestPermissionResult(requestCode, permissions, grantResults);
-    }
 }
