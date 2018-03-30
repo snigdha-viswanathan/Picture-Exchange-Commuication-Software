83bfc5244816017a63f95c472e7b13a6710a74f3
NAFISA SAIDA <nafisasaida826@gmail.com>
2018-03-30 17:21:05 +0530

    Commiting game files

--- app/src/main/java/ssn/codebreakers/pecsinstructor/db/helpers/CardHelper.java
+++ app/src/main/java/ssn/codebreakers/pecsinstructor/db/helpers/CardHelper.java
@@ -9,6 +9,7 @@ import java.util.List;
 import ssn.codebreakers.pecsinstructor.db.LocalDatabase;
 import ssn.codebreakers.pecsinstructor.db.models.Card;
 import ssn.codebreakers.pecsinstructor.helpers.S3Helper;
+import java.util.Random;
 
 public class CardHelper
 {
@@ -22,11 +23,19 @@ public class CardHelper
         localDatabase.close();
     }
 
+
     public static void getCard(Context context, String id)
     {
         LocalDatabase localDatabase = Room.databaseBuilder(context, LocalDatabase.class, "pecsi-local").allowMainThreadQueries().fallbackToDestructiveMigration().build();
         localDatabase.cardDao().get(id);
         localDatabase.close();
+    }
+    public static Card getRandomCardOfCategory(Context context, String categoryId)
+    {
+        List<Card>  cards = getCardsOfCategory(context,categoryId);
+        Random random = new Random();
+        int randomId = random.nextInt(cards.size());
+        return cards.get(randomId);
     }
 
     public static List<Card> getCardsOfCategory(Context context, String categoryId)
---
+++ app/src/main/java/ssn/codebreakers/pecsinstructor/helpers/SpeechHelper.java
@@ -0,0 +1,65 @@
+package ssn.codebreakers.pecsinstructor.helpers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class SpeechHelper
+{
+    public static final int SPEECH_REQUEST_ID = 100;
+
+    TextToSpeech textToSpeech;
+    boolean textToSpeechInitialized = false;
+    Context context;
+    Activity activity;
+
+    public SpeechHelper(Context context, Activity activity)
+    {
+        this.context = context;
+        this.activity = activity;
+        textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    textToSpeechInitialized = true;
+                }
+            }
+        });
+    }
+
+    public void textToSpeech(String text)
+    {
+        if(textToSpeechInitialized)
+        {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+    }
+
+    public void speechToText()
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "speak now");
+        try {
+            activity.startActivityForResult(intent, SPEECH_REQUEST_ID);
+        } catch (Exception a) {
+            Toast.makeText(context, "Sorry. your device does not support speech recognition.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
